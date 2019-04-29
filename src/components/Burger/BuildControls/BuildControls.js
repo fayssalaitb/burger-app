26e692713690f0bd20fe8b94bebd3936656d9b51
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-04-29 22:04:05 +0200

    add controls and cnnected them to the logic and disply the calculated price

--- src/components/Burger/BuildControls/BuildControls.js
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -4,15 +4,24 @@ import BuildControl from "./BuildControl/BuildControl";
 
 const controls = [
   { label: "Salad", type: "salad" },
-  { label: "Bacon", type: "Bacon" },
-  { label: "Cheese", type: "Cheese" },
+  { label: "Bacon", type: "bacon" },
+  { label: "Cheese", type: "cheese" },
   { label: "Meat", type: "meat" }
 ];
 
 const buildControls = props => (
   <div className={classes.BuildControls}>
+    <p>
+      Current Price <strong>{props.price.toFixed(2)}</strong> $
+    </p>
     {controls.map(ctrl => (
-      <BuildControl key={ctrl.label} label={ctrl.label} />
+      <BuildControl
+        key={ctrl.label}
+        label={ctrl.label}
+        added={() => props.ingredientAdded(ctrl.type)}
+        removed={() => props.ingredientRemoved(ctrl.type)}
+        disabled={props.disabledInfo[ctrl.type]}
+      />
     ))}
   </div>
 );
