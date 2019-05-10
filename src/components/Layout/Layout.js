39e8dc94f41941e26d2003204cc4068a488a14d8
fayssals-MacBook-Pro <bouizargane.fayssal@gmail.com>
2019-05-10 02:06:58 +0200

    navigation added

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import Aux from "../../hoc/Aux";
 import classes from "./Layout.module.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const layout = props => (
-  <Aux>
-    <Toolbar />
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  };
+  SideDrawerClosedHandler = () => {
+    this.setState({ showSideDrawer: false });
+  };
+  sideDrawerToggleHandler = () => {
+    this.setState(prevState => {
+      return { showSideDrawer: !prevState.showSideDrawer };
+    });
+  };
+  render() {
+    return (
+      <Aux>
+        <Toolbar DrawerToggleClicked={this.sideDrawerToggleHandler} />
+        <SideDrawer
+          open={this.state.showSideDrawer}
+          closed={this.SideDrawerClosedHandler}
+        />
 
-    <main className={classes.Content}>{props.children}</main>
-  </Aux>
-);
-
-export default layout;
+        <main className={classes.Content}>{this.props.children}</main>
+      </Aux>
+    );
+  }
+}
+export default Layout;
